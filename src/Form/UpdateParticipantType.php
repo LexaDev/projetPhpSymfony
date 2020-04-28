ec91d53f8cdee7c9b712b24a7736aa11665f6ed6
grolandsat <albanuzureau@gmail.com>
2020-04-28 14:00:28 +0200

    creation formulaire UpdateParticipantType

---
+++ src/Form/UpdateParticipantType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Participant;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UpdateParticipantType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('roles')
+            ->add('password')
+            ->add('firstName')
+            ->add('lastName')
+            ->add('phoneNumber')
+            ->add('email')
+            ->add('actif')
+            ->add('site')
+            ->add('outingsParticipate')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Participant::class,
+        ]);
+    }
+}
