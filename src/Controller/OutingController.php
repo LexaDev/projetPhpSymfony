e55a49cd56e263ae5e65328bee877e4c4b5f179f
grolandsat <albanuzureau@gmail.com>
2020-04-30 11:03:48 +0200

    ajout possibilité de s'inscrire

--- src/Controller/OutingController.php
+++ src/Controller/OutingController.php
@@ -8,6 +8,7 @@ use App\Form\OutingType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OutingController extends AbstractController
@@ -26,5 +27,34 @@ class OutingController extends AbstractController
         return $this->render('outing/createOuting.html.twig', [
             'outingForm' => $outingForm->createView()
         ]);
+    }
+
+    /**
+     *
+     * @Route("/subscribe/{id}", name="outing_subscribe",methods={"GET"})
+     */
+    public function subscribe($id,EntityManagerInterface $em, Request $request)
+    {
+        $user = $this->getUser();
+        //$id = $request->get('id');
+        dump($id);
+        $outingRepo = $this->getDoctrine()->getRepository(Outing::class);
+        $outing = $outingRepo->find($id);
+        if (isset($outing))
+        {
+            if (($outing->isParticipant($user)))
+            {
+                return new Response('User deja participant',Response::HTTP_FORBIDDEN);
+            }
+            $outing->addParticipant($user);
+            $em->persist($outing);
+            $em->flush();
+
+
+            return new Response('OK',Response::HTTP_OK);
+        }else{
+            return new Response('outing not find',Response::HTTP_NOT_FOUND);
+        }
+
     }
 }
