a680e044d0f4d5ff058f9dae30bf978e7fc1dd16
Alexandra <alexandra.peyrical2019@campus-eni.fr>
2020-05-01 14:40:03 +0200

    fonctionnalité et vue 'affichage d'une sortie'

--- src/Controller/OutingController.php
+++ src/Controller/OutingController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 use App\Entity\Location;
 use App\Entity\Outing;
 use App\Form\OutingType;
+use App\Repository\OutingRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -100,19 +101,31 @@ class OutingController extends AbstractController
             } else {
                 return new Response('Cette sortie n\'est plus disponible', Response::HTTP_NOT_FOUND);
             }
-        }}
-
-        /**
-         * @Route("/createOuting/{id}", name="create_outing_id")
-         */
-        public function idLieu($id)
-        {
-
-            $locationRepo = $this->getDoctrine()->getRepository(Location::class);
-            $location = $locationRepo->findLocationandCity($id);
-
-              return new JsonResponse(['infosLieu'=>$location]);
         }
     }
+
+    /**
+     * @Route("/createOuting/{id}", name="create_outing_id")
+     */
+    public function idLieu($id)
+    {
+
+        $locationRepo = $this->getDoctrine()->getRepository(Location::class);
+        $location = $locationRepo->findLocationandCity($id);
+
+          return new JsonResponse(['infosLieu'=>$location]);
+    }
+
+    /**
+     * @Route("viewOuting/{id}", name="view_outing", requirements={"id": "\d+"})
+     */
+    public function viewOuting($id, OutingRepository $repository)
+    {
+        $outing = $repository->find($id);
+        return $this->render('outing/viewOuting.html.twig', [
+            'outing' => $outing
+        ]);
+    }
+}
 
 
